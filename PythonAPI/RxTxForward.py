41f477b2cd5e5653014e2187a324a2a8e3177fc6
Max <Max>
2018-01-24 14:07:30 +0100

    add a command line reader to send strings to the PHY in addition to the loopback

--- PythonAPI/RxTxForward.py
+++ PythonAPI/RxTxForward.py
@@ -4,6 +4,7 @@ sys.path.append(r"C:\Users\admin\Documents\Python\nifpga-python")
 import time
 import numpy as np
 import os
+import threading
 
 from nifpga import Session
 import nifpga
@@ -19,8 +20,29 @@ def parseArgs():
     args = parser.parse_args()
     return args.rio, args.packet_size
 
+def forwardFunc(stopEvent, txFifo, rxFifo, packetSize, printIt):
+    while not stopEvent.isSet():
+        try:
+            data, remaining = rxFifo.read(packetSize, timeout_ms=200)
+            txFifo.write(data)
+            if printIt:
+                for d in data:
+                    try:
+                        sys.stdout.write(chr(d))
+                        pass
+                    except RuntimeError:
+                        pass                   
+        except nifpga.FifoTimeoutError:
+            pass
+    print ("Finishing thread")
 
-    
+def writeStringToFifo(line, fifo):
+    line = "\n"*2+line+"\n"
+    line = line * 3
+    data = [ord(c) for c in line]
+    fifo.write(data, timeout_ms=200)
+    pass
+
 
 def main(RIO, packetSize):
     bitfilename = os.path.abspath(os.path.join(os.path.dirname(__file__), '../Builds/GFDM FPGA Toplevel for 40MHz USRP.lvbitx'))
@@ -30,26 +52,28 @@ def main(RIO, packetSize):
     txFifoName = 'Needed Resources.grsc\\TX.data in.H2T'
 
     with Session(bitfilename, RIO) as session:
-
-            txFifo = session.fifos[txFifoName]
-            txFifo.configure(10000000)
-            txFifo.start()
-            rxFifo = session.fifos[rxFifoName]
-            rxFifo.configure(10000000)
-            rxFifo.start()
-
-            while True:
-                try:
-                    data, remaining = rxFifo.read(packetSize, timeout_ms=200)
-                    for d in data:
-                        try:
-                            sys.stdout.write(chr(d))
-                        except RuntimeError:
-                            pass
-                    
-                    txFifo.write(data)
-                except nifpga.FifoTimeoutError:
-                    pass
+        txFifo = session.fifos[txFifoName]
+        txFifo.configure(10000000)
+        txFifo.start()
+        rxFifo = session.fifos[rxFifoName]
+        rxFifo.configure(10000000)
+        rxFifo.start()
+            
+        stopEvent = threading.Event()
+        try:
+            T = threading.Thread(target=forwardFunc, args=(stopEvent, txFifo, rxFifo, packetSize, False))
+            T.start()
+            print ("Forwarding started. Type to send extra commands.\nType X to exit.")
+            for l in sys.stdin:
+                l = l.strip()
+                if l == 'X':
+                    break
+                writeStringToFifo(l, txFifo)
+            pass
+        finally:
+            stopEvent.set()
+            T.join()
+            
                     
 if __name__ == '__main__':
     main(*parseArgs())
